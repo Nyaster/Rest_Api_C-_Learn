cdcc6c523be0e0b7e00d3abb8330b7a04a9ebf36
s26842 <s26842@pjwstk.edu.pl>
2024-04-12 04:21:46 +0200

    created project architecture, add logick for secrets

---
+++ RestApiLearn/RestApiLearn/Models/Animal.cs
@@ -0,0 +1,9 @@
+﻿using System.Security.Cryptography;
+
+namespace RestApiLearn.Models;
+
+public class Animal
+{
+    private int id;
+    private string name;
+}
--- RestApiLearn/RestApiLearn/Program.cs
+++ RestApiLearn/RestApiLearn/Program.cs
@@ -5,13 +5,13 @@ public class Program
     public static void Main(string[] args)
     {
         var builder = WebApplication.CreateBuilder(args);
-
-        // Add services to the container.
-        builder.Services.AddAuthorization();
-
+        var configuration = new ConfigurationBuilder()
+            .AddJsonFile("secrets.json", optional: true, reloadOnChange: true).Build();
+        builder.Configuration.AddConfiguration(configuration);
         // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
         builder.Services.AddEndpointsApiExplorer();
         builder.Services.AddSwaggerGen();
+        builder.Services.AddControllers().AddXmlSerializerFormatters();
 
         var app = builder.Build();
 
@@ -21,31 +21,10 @@ public class Program
             app.UseSwagger();
             app.UseSwaggerUI();
         }
-
+        string connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+        Console.WriteLine(connectionString);
         app.UseHttpsRedirection();
-
-        app.UseAuthorization();
-
-        var summaries = new[]
-        {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-        };
-
-        app.MapGet("/weatherforecast", (HttpContext httpContext) =>
-            {
-                var forecast = Enumerable.Range(1, 5).Select(index =>
-                        new WeatherForecast
-                        {
-                            Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                            TemperatureC = Random.Shared.Next(-20, 55),
-                            Summary = summaries[Random.Shared.Next(summaries.Length)]
-                        })
-                    .ToArray();
-                return forecast;
-            })
-            .WithName("GetWeatherForecast")
-            .WithOpenApi();
-
+        app.MapControllers();
         app.Run();
     }
 }
